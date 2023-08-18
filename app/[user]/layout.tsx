e6d47a4020ad5c6768de8e0d22401f6001e6f71e
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-08-18 16:33:03 +0900

    hotfix

--- app/[user]/layout.tsx
+++ app/[user]/layout.tsx
@@ -6,18 +6,9 @@ import Navbar from "@/components/navbar";
 
 export default async function Layout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { user: string };
 }) {
-  const session = await getServerSession(authOptions);
-
-  if (session!.user.id !== params.user) {
-    if (process.env.NEXT_PUBLIC_ENV === "Production")
-      throw new Error("Unauthorized");
-  }
-
   return (
     <div className="relative flex h-auto text-[#333333]">
       <Navbar />
