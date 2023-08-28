7beb16a6994933088a3a62a32eb4d2ff1a9cc69a
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-08-28 12:21:58 +0900

    resolved

--- app/@modal/(.)upload/page.tsx
+++ app/@modal/(.)upload/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { supabase, supabaseAuth } from "@/lib/database";
 import UploadModal from "@/components/modal/upload-modal";
+import { getMostUsedTags } from "@/lib/supabase";
 
-export default function Upload(props: any) {
+export default async function Upload(props: any) {
   const { params } = props;
 
+  const mostUsedTags = await getMostUsedTags();
+
   return (
-    <UploadModal />
+    <UploadModal mostUsedTags={mostUsedTags} />
   );
 }
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,6 +6,32 @@ export const generatePublicUrl = (storage: string, path: string) => {
   return `${supabasePublic}/${storage}/${path}`;
 };
 
+export const getMostUsedTags = async () => {
+  const { data, error } = await supabase
+    .from("tags")
+    .select("*", { count: "exact" });
+
+  const countByGroupTag: any = {};
+  data!.forEach((row) => {
+    const tag = row.tag!;
+    if (countByGroupTag[tag]) {
+      countByGroupTag[tag]++;
+    } else {
+      countByGroupTag[tag] = 1;
+    }
+  });
+  const countArray = Object.entries(countByGroupTag);
+  countArray.sort((a: any, b: any) => b[1] - a[1]);
+
+  const slicedCountByGroupTag = Object.fromEntries(countArray.slice(0, 5));
+
+  const options = Object.keys(slicedCountByGroupTag).map((tag: any) => {
+    return { value: tag, label: tag };
+  });
+
+  return options;
+};
+
 export const getProfile = async (id: string) => {
   const { data, error } = await supabase
     .from("profiles")
