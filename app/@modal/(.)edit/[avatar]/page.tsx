85d371cbccc7133c5c9e441b8de2bbd733b20f14
신규화 <quashine@icloud.com>
2023-08-28 13:08:52 +0900

    hmmmmm

--- app/@modal/(.)edit/[avatar]/page.tsx
+++ app/@modal/(.)edit/[avatar]/page.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { supabase, supabaseAuth } from "@/lib/database";
 import EditModal from "@/components/modal/edit-modal";
 import { createModelUrl, getAvatar, getMostUsedTags } from "@/lib/supabase";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+export const revalidate = 0;
+
+
 export default async function Edit(props: any) {
   const { params } = props;
   const session = await getServerSession(authOptions);
 
-  const avatarData = getAvatar(params.avatar);
-  const [avatar] = await Promise.all([avatarData]);
-
+  const avatar = await getAvatar(params.avatar);
   const modelUrl = await createModelUrl(session?.user.id!, avatar?.vrm);
-
   const mostUsedTags = await getMostUsedTags();
 
   return (
