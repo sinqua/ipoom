9241b915a44477bfc1d012bb28eaba5713ac8229
신규화 <quashine@icloud.com>
2023-08-28 15:57:20 +0900

    really notghing.

--- app/@modal/(.)edit/[avatar]/page.tsx
+++ app/@modal/(.)edit/[avatar]/page.tsx
@@ -8,11 +8,11 @@ export const revalidate = 0;
 
 export default async function Edit(props: any) {
   const { params } = props;
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  const avatar = await getAvatar(params.avatar);
-  const modelUrl = await createModelUrl(session?.user.id!, avatar?.vrm);
-  const mostUsedTags = await getMostUsedTags();
+  // const avatar = await getAvatar(params.avatar);
+  // const modelUrl = await createModelUrl(session?.user.id!, avatar?.vrm);
+  // const mostUsedTags = await getMostUsedTags();
 
   return (
     <div>Hello</div>
