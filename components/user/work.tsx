2a653858ea69ee2f43641365c8cba9da57e17205
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-08-31 14:29:00 +0900

    finish

--- components/user/work.tsx
+++ components/user/work.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Cover from "./cover";
 
 export default function Work({ user, avatar }: { user: any; avatar: any }) {
   const today = new Date();
@@ -11,39 +12,48 @@ export default function Work({ user, avatar }: { user: any; avatar: any }) {
   const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
   const betweenTimeWeek = Math.floor(betweenTime / 60 / 24 / 7);
 
+  console.log("avatar", avatar);
+
   return (
-    <Link
-      href={`/${user}/avatar/${avatar.id}`}
-      className="relative w-full dt:h-[240px] ph:h-[233px] h-[240px] rounded-[8px] overflow-hidden cursor-pointer"
-    >
-      <Image
-        src={avatar.thumbnailUrl}
-        width={512}
-        height={512}
-        className="object-cover w-full h-full"
-        alt=""
-      />
-      <div className="absolute bottom-0 flex flex-col justify-between items-center w-full h-[61px] p-[8px] bg-black/20 backdrop-blur-[3px] text-[#FFFFFF]">
-        <div className="flex justify-between w-full">
-          <p>{avatar.name}</p>
-          <p>
-            {betweenTimeWeek > 0
-              ? `${betweenTimeWeek}주 전`
-              : today.getDate() === createdDate.getDate()
-              ? "오늘"
-              : betweenTimeDay === 0
-              ? "1일 전"
-              : `${betweenTimeDay}일 전`}
-          </p>
+    <div className="relative w-full dt:h-[240px] ph:h-[233px] h-[240px] rounded-[8px] overflow-hidden">
+      <Link
+        href={`/${user}/avatar/${avatar.id}`}
+        className="relative w-full h-full cursor-pointer rounded-[8px] overflow-hidden"
+      >
+        <Image
+          src={avatar.thumbnailUrl}
+          width={512}
+          height={512}
+          className="object-cover w-full h-full"
+          alt=""
+        />
+        <div className="absolute bottom-0 flex flex-col justify-between items-center w-full h-[61px] p-[8px] bg-black/20 backdrop-blur-[3px] text-[#FFFFFF]">
+          <div className="flex justify-between items-center w-full">
+            <p className="text-[16px] font-semibold">{avatar.name}</p>
+            <p className="text-[12px]">
+              {betweenTimeWeek > 0
+                ? `${betweenTimeWeek}주 전`
+                : today.getDate() === createdDate.getDate()
+                ? "오늘"
+                : betweenTimeDay === 0
+                ? "1일 전"
+                : `${betweenTimeDay}일 전`}
+            </p>
+          </div>
+          <div className="w-full">
+            <p className="text-[12px] whitespace-nowrap overflow-hidden text-ellipsis">
+              {avatar.tags.map((item: any, index: any) => {
+                return index === 0
+                  ? `#${item.tag}`
+                  : `\u00A0\u00A0#${item.tag}`;
+              })}
+            </p>
+          </div>
         </div>
-        <div className="w-full">
-          <p className="text-[12px] whitespace-nowrap overflow-hidden text-ellipsis">
-            {avatar.tags.map((item: any, index: any) => {
-              return index === 0 ? `#${item.tag}` : `\u00A0\u00A0#${item.tag}`;
-            })}
-          </p>
-        </div>
-      </div>
-    </Link>
+      </Link>
+      {!avatar.optimized && (
+        <Cover />
+      )}
+    </div>
   );
 }
