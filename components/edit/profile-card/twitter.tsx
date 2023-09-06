d6a2e55c221aeb8184990e5ffd2895097e46e0aa
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-09-06 12:45:44 +0900

    hotfix

--- components/edit/profile-card/twitter.tsx
+++ components/edit/profile-card/twitter.tsx
@@ -19,7 +19,7 @@ export default function Twitter({ link }: { link: any }) {
           <input
             type="text"
             ref={inputRef}
-            className="grow h-full outline-none text-sm"
+            className="w-full h-full outline-none text-sm"
             placeholder="트위터 URL을 입력해주세요."
             defaultValue={link ? link : ""}
           ></input>
