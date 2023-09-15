81cd7dfce449f3bb31e06abd15b82fe79ec00d46
신규화 <quashine@icloud.com>
2023-09-15 18:24:52 +0900

    google analytics

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,16 +1,19 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const GA_TRACKING_URL = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`
+  
   return (
     <div className="container">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-J942NQQLB8" />
+      <Script src={GA_TRACKING_URL} />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'G-J942NQQLB8');
+          gtag('config', ${GA_TRACKING_ID});
         `}
       </Script>
     </div>
