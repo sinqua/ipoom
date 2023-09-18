7e7801c59e0c196aabdfcb2200a9351828a51f76
신규화 <quashine@icloud.com>
2023-09-18 15:00:57 +0900

    change channel

--- components/discord.tsx
+++ components/discord.tsx
@@ -2,7 +2,7 @@ export default function Discord() {
   const text = `<script src='https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' async defer>
   new Crate({
       server: '1125351036740194336', // 무피
-      channel: '1125351038170443838' // #😀일반
+      channel: '1125364635638452234' //
   })
 </script>`;
   return <div dangerouslySetInnerHTML={{ __html: text }}></div>;
