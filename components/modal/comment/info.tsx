16a9395595ac28d13b05a623e0b3288fd8ef4301
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-09-21 13:03:11 +0900

    resolved

--- components/modal/comment/info.tsx
+++ components/modal/comment/info.tsx
@@ -1,23 +1,9 @@
 "use client";
 import { addReply } from "@/lib/supabase";
 import { useRef, useState } from "react";
-
 import { getTimeAgo } from "@/lib/utils";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Separator } from "@/components/ui/separator";
-
 import { usePathname, useRouter } from "next/navigation";
+import AlertLogin from "@/components/aler-login";
 
 interface CommentInfoProps {
   userId: any;
@@ -32,10 +18,8 @@ export default function CommentInfo({
   comment,
   replies,
   setReplies,
-  setIsShowReply
+  setIsShowReply,
 }: CommentInfoProps) {
-  const router = useRouter();
-  const pathname = usePathname();
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
   const [isOpen, setIsOpen] = useState(false);
@@ -90,28 +74,7 @@ export default function CommentInfo({
           </div>
         )}
       </div>
-      <AlertDialog open={isOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>로그인이 필요한 서비스입니다.</AlertDialogTitle>
-            <AlertDialogDescription>
-              로그인 페이지로 이동합니다.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <Separator />
-          <AlertDialogFooter>
-            <AlertDialogAction
-              onClick={() => router.push(`/login?callbackUrl=${pathname}`)}
-            >
-              이동
-            </AlertDialogAction>
-            <Separator orientation="vertical" />
-            <AlertDialogCancel onClick={() => setIsOpen(false)}>
-              취소
-            </AlertDialogCancel>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <AlertLogin isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 }
--- components/modal/like-button.tsx
+++ components/modal/like-button.tsx
@@ -1,25 +1,11 @@
 "use client";
 import { addLike, deleteLike } from "@/lib/supabase";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import AlertLogin from "../aler-login";
 
 import Image from "next/image";
 import HeartLineImg from "@/app/assets/images/heart_line.svg";
 import HeartFillImg from "@/app/assets/images/heart_fill.svg";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Separator } from "@/components/ui/separator";
-
-import { usePathname, useRouter } from "next/navigation";
 
 interface LikeButtonProps {
   userId: any;
@@ -32,9 +18,6 @@ export default function LikeButton({
   avatarId,
   likes,
 }: LikeButtonProps) {
-  const router = useRouter();
-  const pathname = usePathname();
-
   const checkLikeStatus = (): Boolean => {
     if (!userId) return false;
 
@@ -82,28 +65,7 @@ export default function LikeButton({
         />
         <p className="font-semibold text-[14px] text-[#FF4E4E]">{likeCount}</p>
       </div>
-      <AlertDialog open={isOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>로그인이 필요한 서비스입니다.</AlertDialogTitle>
-            <AlertDialogDescription>
-              로그인 페이지로 이동합니다.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <Separator />
-          <AlertDialogFooter>
-            <AlertDialogAction
-              onClick={() => router.push(`/login?callbackUrl=${pathname}`)}
-            >
-              이동
-            </AlertDialogAction>
-            <Separator orientation="vertical" />
-            <AlertDialogCancel onClick={() => setIsOpen(false)}>
-              취소
-            </AlertDialogCancel>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <AlertLogin isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 }
