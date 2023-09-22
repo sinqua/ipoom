04c56f4f43e76a405e1dc306049e5e38b94fdb53
신규화 <quashine@icloud.com>
2023-09-22 11:13:05 +0900

    return null if no ga id

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -4,6 +4,8 @@ export default function GoogleAnalytics() {
   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
   const GA_TRACKING_URL = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`
   
+  if(!GA_TRACKING_ID) return null
+
   return (
     <div className="container">
       <Script src={GA_TRACKING_URL} />
@@ -13,7 +15,7 @@ export default function GoogleAnalytics() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', '${GA_TRACKING_ID}');
+          gtag('config', 'G-FRH6AH1D9Z');
         `}
       </Script>
     </div>
