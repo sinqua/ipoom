a0c70b543705b571e0066a629505e912e80ed261
신규화 <quashine@icloud.com>
2023-09-22 11:39:24 +0900

    keep no data at env

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,14 +1,11 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
-  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
-  const GA_TRACKING_URL = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`
-  
-  if(!GA_TRACKING_ID) return null
+  if(!process.env.NEXT_PUBLIC_GA_ID) return null
 
   return (
     <div className="container">
-      <Script src={GA_TRACKING_URL} />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-FRH6AH1D9Z" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
