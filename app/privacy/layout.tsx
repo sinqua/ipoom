c1619b4738432cd9f188b93a7419b785cfde60a3
신규화 <quashine@icloud.com>
2023-09-22 14:08:10 +0900

    next 13.5

--- app/privacy/layout.tsx
+++ app/privacy/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/basic-layout/footer";
 import Navbar from "@/components/navbar";
-import { Toaster } from "@/components/ui/toaster";
 
 export default async function Layout({
   children,
