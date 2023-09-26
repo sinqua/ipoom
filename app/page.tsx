c0fade41030ae0a8b9f3fcdedd3db75a380dd0c5
신규화 <quashine@icloud.com>
2023-09-26 17:15:18 +0900

    remove carousel suspense

--- app/page.tsx
+++ app/page.tsx
@@ -44,10 +44,8 @@ export default async function Main() {
             <Header tags={tags} />
             <div className="flex flex-col items-center w-full grow">
               {/* <Tags tags={tags} /> */}
-              <Suspense fallback={<div>Loading...</div>}>
-                <Carousel />
-              </Suspense>
-              <Suspense>
+              <Carousel />
+              <Suspense fallback={<div>cardssssssss...</div>}>
                 <div className="relative flex flex-col dt:max-w-[1008px] w-full h-full dt:px-0 px-[16px] px:pt-[60px] pt-[40px] pb-[80px] space-y-[64px]">
                   <Popular avatars={popularAvatars} />
                   {session && <Follow avatars={followAvatars} />}
