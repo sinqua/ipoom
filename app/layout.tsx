bea25339cf8d51f1de64d11ef9a1273ad97bdfa1
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-09-27 17:03:49 +0900

    hotfix toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import AuthProvider from "@/components/auth-provider";
 import GoogleAnalytics from "@/components/google-analytics";
 import Discord from "@/components/discord";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             <AuthProvider>
               {children}
               {modal}
+              <Toaster />
             </AuthProvider>
             <Discord />
           </main>
@@ -38,7 +40,8 @@ export const metadata: Metadata = {
     default: "무피 - 3D 아바타 커뮤니케이션 플랫폼",
     template: "%s | 무피",
   },
-  description: "당신이 만든 캐릭터가 더 많은 사람을 만나는 무피. 어떤 기기에서도 생동감 있고 자연스러운 캐릭터를 보여줍니다.",
+  description:
+    "당신이 만든 캐릭터가 더 많은 사람을 만나는 무피. 어떤 기기에서도 생동감 있고 자연스러운 캐릭터를 보여줍니다.",
   applicationName: "무피",
   keywords: [
     "3D",
@@ -52,12 +55,13 @@ export const metadata: Metadata = {
     "VRChat",
     "VRM",
     "VRC",
-    "커미션"
+    "커미션",
   ],
   metadataBase: new URL("https://moopi.offing.me/"),
   openGraph: {
     title: "무피 - 3D 아바타 커뮤니케이션 플랫폼",
-    description: "당신이 만든 캐릭터가 더 많은 사람을 만나는 무피. 어떤 기기에서도 생동감 있고 자연스러운 캐릭터를 보여줍니다.",
+    description:
+      "당신이 만든 캐릭터가 더 많은 사람을 만나는 무피. 어떤 기기에서도 생동감 있고 자연스러운 캐릭터를 보여줍니다.",
     url: "https://moopi.offing.me/",
     siteName: "무피",
     images: [
