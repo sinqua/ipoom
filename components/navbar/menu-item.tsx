cd4aa812ae86ae1c366f9cb8d7eeebfc3ea2003f
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-10-05 17:23:14 +0900

    remove image tag in link

--- components/navbar/menu-item.tsx
+++ components/navbar/menu-item.tsx
@@ -15,7 +15,7 @@ export default function Item({
       href={url}
       className="flex items-center w-full h-[48px] px-[32px] space-x-[16px] bg-white hover:bg-[#F6F6F6] cursor-pointer"
     >
-      <Image src={imgSrc} className="w-[24px] h-[24px]" alt="" priority />
+      {/* <Image src={imgSrc} className="w-[24px] h-[24px]" alt="" priority /> */}
       <p className="text-[16px]">{children}</p>
     </Link>
   );
