8d564d7ec69adf9de07c914df413c57544182c2e
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-10-05 17:46:46 +0900

    wrap with div

--- components/navbar/menu-item.tsx
+++ components/navbar/menu-item.tsx
@@ -4,19 +4,18 @@ import Link from "next/link";
 export default function Item({
   children,
   imgSrc,
-  url
+  url,
 }: {
   children: React.ReactNode;
   imgSrc: string;
   url: string;
 }) {
   return (
-    <Link
-      href={url}
-      className="flex items-center w-full h-[48px] px-[32px] space-x-[16px] bg-white hover:bg-[#F6F6F6] cursor-pointer"
-    >
-      {/* <Image src={imgSrc} className="w-[24px] h-[24px]" alt="" priority /> */}
-      <p className="text-[16px]">{children}</p>
+    <Link href={url}>
+      <div className="flex items-center w-full h-[48px] px-[32px] space-x-[16px] bg-white hover:bg-[#F6F6F6] cursor-pointer">
+        <Image src={imgSrc} className="w-[24px] h-[24px]" alt="" priority />
+        <p className="text-[16px]">{children}</p>
+      </div>
     </Link>
   );
 }
