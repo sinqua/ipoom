99a9a0a6dd74be70199785f470e4dae1367c77bf
_js__yong <50312989+sunyongj1004@users.noreply.github.com>
2023-10-16 16:08:03 +0900

    asdf

--- components/login/google-login.tsx
+++ components/login/google-login.tsx
@@ -13,16 +13,28 @@ export default function GoogleLogin() {
   const callbackUrl = ("/verify?callbackUrl=" +
     (searchParams.get("callbackUrl") ?? "/")) as string;
 
-    const signInWithGoogle = async () => {
-      await supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-          redirectTo: `${location.origin}/auth/callback?urlTo=${callbackUrl}`,
-        },
-      });
-  
-      router.refresh();
-    };
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
+  const signInWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${getURL()}/auth/callback?urlTo=${callbackUrl}`,
+      },
+    });
+
+    router.refresh();
+  };
 
   return (
     <div
