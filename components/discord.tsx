01a08f8aa70fe9696ba178271fa50c2b40d1c5a6
신규화 <quashine@icloud.com>
2023-10-16 16:35:18 +0900

    strategy for script element

--- components/discord.tsx
+++ components/discord.tsx
@@ -1,5 +1,5 @@
 export default function Discord() {
-  const text = `<script src='https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' async defer>
+  const text = `<script src='https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' async defer strategy="afterInteractive">
   new Crate({
       server: '1125351036740194336', // 무피
       channel: '1125364635638452234' //
--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -5,8 +5,8 @@ export default function GoogleAnalytics() {
 
   return (
     <div className="container">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-J942NQQLB8" />
-      <Script id="google-analytics">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-J942NQQLB8" strategy="afterInteractive"/>
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
