41b1ee6e69f8a23fac2076edef19e7f92744928b
신규화 <quashine@icloud.com>
2023-10-18 14:53:31 +0900

    useeffect for redirect

--- components/login/redirect.tsx
+++ components/login/redirect.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Redirect() {
   const router = useRouter();
-  router.push("/home");
+
+  useEffect(() => {
+    router.push("/home");
+  }, []);
 
   return <></>;
 }
