5dcc53adfb6b4db6bc2a8d12cca46d513a206df1
신규화 <quashine@icloud.com>
2023-10-18 12:26:13 +0900

    prefetch false

--- components/navbar/menu-item.tsx
+++ components/navbar/menu-item.tsx
@@ -11,7 +11,7 @@ export default function Item({
   url: string;
 }) {
   return (
-    <Link href={url}>
+    <Link href={url} prefetch={false}>
       <div className="flex items-center w-full h-[48px] px-[32px] space-x-[16px] bg-white hover:bg-[#F6F6F6] cursor-pointer">
         <Image src={imgSrc} className="w-[24px] h-[24px]" alt="" priority />
         <p className="text-[16px]">{children}</p>
